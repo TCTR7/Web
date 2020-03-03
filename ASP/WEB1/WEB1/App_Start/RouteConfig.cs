8b39078adc4462fd3f1c6c5f565b957155a94d1a
Tran Cao Trung <caotrung4691@gmail.com>
2020-03-03 20:46:54 +0700

    Add first project

---
+++ ASP/WEB1/WEB1/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WEB1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Web1",
+                url: "{controller}/{action}/{ten}/{tuoi}",
+                defaults: new { controller = "Web1", action = "ChaoMung" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ASP/WEB1/WEB1/Controllers/Web1Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEB1.Controllers
+{
+    public class Web1Controller : Controller
+    {
+        // GET: Web1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string ChaoMung(string ten, int tuoi = 1)
+        {
+            return HttpUtility.HtmlEncode("Xin chào " + ten + ". Tuổi của bạn là : " + tuoi);
+        }
+
+        public ActionResult Hello(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }
+    }
+}
