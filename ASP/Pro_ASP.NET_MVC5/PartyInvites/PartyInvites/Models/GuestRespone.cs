7cf1d4fb50e3f654b1f8ab699df90c58ecf94892
Tran Cao Trung <caotrung4691@gmail.com>
2020-03-19 22:57:02 +0700

    + Add Project

---
+++ ASP/Pro_ASP.NET_MVC5/PartyInvites/PartyInvites/Models/GuestRespone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestRespone
+    {
+        [Required(ErrorMessage ="please enter your Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "please enter your Email adress")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage ="please enter a valid email adress")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "please enter your Phone")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage ="please specify whether you'll attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
